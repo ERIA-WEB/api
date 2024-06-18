2e15f45ab365d444673c2efecf391e272ff5f75b
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-06-18 15:30:12 +0700

    Controllers: Update Mega Menu Controller API

--- app/Http/Controllers/MegaMenuController.php
+++ app/Http/Controllers/MegaMenuController.php
@@ -37,4 +37,32 @@ class MegaMenuController extends Controller
 
         return response()->json($data, 200);
     }
+
+    public function renderMenuAseanProgrammes(Request $request)
+    {
+        $published = 1;
+        $tag = 'ASEAN';
+
+        $limit = 4;
+
+        $data = $this->mega_menu->getMenuAseanProgrammes($tag, $limit);
+        if (empty($data) OR count($data) == 0) {
+            return response()->json(['message' => 'Data not found'], 404);
+        }
+
+        return response()->json($data, 200);
+    }
+
+    public function renderMenuProgrammes(Request $request)
+    {
+        $published = 1;
+        $type = 'categories';
+
+        $data = $this->mega_menu->getMenuProgrammes($type, $published);
+        if (empty($data) OR count($data) == 0) {
+            return response()->json(['message' => 'Data not found'], 404);
+        }
+
+        return response()->json($data, 200);
+    }
 }
