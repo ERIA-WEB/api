68512bf98428167774e0e60188abfa6a331c0f7f
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-06-18 15:30:24 +0700

    Models: Update Mega Menu Models API

--- app/Models/MegaMenuModel.php
+++ app/Models/MegaMenuModel.php
@@ -27,4 +27,30 @@ class MegaMenuModel
                 
         return $data;
     }
+
+    public function getMenuAseanProgrammes($tag, $limit)
+    {
+        $data = DB::connection('mysql2')
+                ->table('articles')
+                ->where('tags', 'LIKE', '%'.$tag.'%')
+                ->where('image_name', '!=', '')
+                ->orderBy('article_id', 'DESC')
+                ->limit($limit)
+                ->get();
+
+        return $data;
+    }
+
+    public function getMenuProgrammes($type, $published)
+    {
+        $data = DB::connection('mysql2')
+                ->table('categories')
+                ->select('*')
+                ->where('category_type', $type)
+                ->where('published', $published)
+                ->orderBy('order_id', 'ASC')
+                ->get();
+
+        return $data;
+    }
 }
