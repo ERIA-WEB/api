1534c6836e0769d0056a45a0687749a25ccb88d0
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-06-18 13:45:17 +0700

    Views: Update page home documentation url API

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -356,7 +356,11 @@ th {
             <td></td>
             <td>Mega Menu</td>
             <td></td>
-            <td></td>
+            <td>
+                <a href="{{ env('APP_URL') }}get/research-areas/topic" target="_blank">
+                    {{ env('APP_URL') }}/get/research-areas/topic
+                </a>
+            </td>
             <td></td>
         </tr>
         <tr>
